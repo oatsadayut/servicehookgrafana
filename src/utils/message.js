47cc79cf7e2f401e2b616cffc8d36bd2d0418bc0
Oatsadayut <ice.oatsadayut@gmail.com>
2022-11-18 16:07:34 +0700

    Fix Format Alert

--- src/utils/message.js
+++ src/utils/message.js
@@ -2,14 +2,22 @@
 const setMessage = (data) => {
     let annotations = data.annotations;
     let value = setValue(data.valueString)
-    let msg = `${annotations.description} \n ${annotations.summary} \n ค่าปัจจุบัน = ${value}`
+    let metric = setMetric(data.valueString)
+    let msg = `${metric} \n ${annotations.description} \n ${annotations.summary} \n ค่าปัจจุบัน = ${value}`
     return msg
 }
 
 const setValue = (value) => { 
-    let resultArr = value.split(" ");
-    let result = resultArr[3].substr(6,12)
-    return result
+    let resultArr = value.split("'");
+    let resultArrValue = resultArr[4].split(" ")
+    let resultValue = resultArrValue[2].substr(6,12) //value
+    return resultValue
+}
+
+const setMetric = (value) => { 
+    let resultArr = value.split("'")
+    let metric = resultArr[3]
+    return metric
 }
 
 module.exports = setMessage;
